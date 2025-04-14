2a6ec0f3267d0e266b66c0824185b0dcdf3b023b
José Baldino <josemanuelbaldino@gmail.com>
2025-04-14 10:28:42 +0100

    add board and player

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-console.log("hello world")
+const playerElm = document.getElementById('player');
+
+let positionX = 0
+let positionY = 0
+
+document.addEventListener(`keydown` , (e) => {
+    if (e.code === `ArrowUp`) {
+        positionY+=3
+        playerElm.style.bottom = `${positionY}px`
+    } else if (e.code === `ArrowDown`) {
+        positionY-=3
+        playerElm.style.bottom = `${positionY}px`
+    } else if (e.code === `ArrowLeft`) {
+        positionX-=3
+        playerElm.style.left = `${positionX}px`
+    } else if (e.code === `ArrowRight`) {
+        positionX+=3
+        playerElm.style.left = `${positionX}px`
+    }
+})
